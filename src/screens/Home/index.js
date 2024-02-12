ab6993c7e03e21fe31b03f0842bb18b8d7dea499
menezes-dev <menezes.icet@gmail.com>
2024-02-12 11:59:57 -0400

    feat: adição dos botões da homepage

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,16 +1,20 @@
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import styles from "./styles";
 
 const Home = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.title}>
-        <Text>Seja bem vindo ao MySoil</Text>
-        <Text>Seu app de informações sobre agricultura de baixo carbono</Text>
+      <View style={styles.containerTitles}>
+        <Text style={[styles.title, styles.principal]}>
+          Seja bem vindo ao MySoil
+        </Text>
+        <Text style={[styles.title, styles.secondary]}>
+          Seu app de informações sobre agricultura de baixo carbono
+        </Text>
       </View>
-      <View style={styles.buttons}>
-        <Text>Botão 1</Text>
-        <Text>Botão 2</Text>
+      <View style={styles.containerButtons}>
+        <TouchableOpacity style={styles.button}>Botão 1</TouchableOpacity>
+        <TouchableOpacity style={styles.button}>Botão 2</TouchableOpacity>
       </View>
       <View>
         <Image source={require("../../assets/tree.png")} style={styles.image} />
--- src/screens/Home/styles.js
+++ src/screens/Home/styles.js
@@ -1,9 +1,16 @@
 import { StyleSheet } from "react-native";
+import theme from "../../theme/theme";
 
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     justifyContent: "space-around",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+
+  containerImage: {
+    backgroundColor: theme.colors.darkGreen,
   },
 
   image: {
@@ -15,18 +22,45 @@ const styles = StyleSheet.create({
     borderColor: "black",
   },
 
-  title: {
+  containerTitles: {
+    width: "70%",
     alignItems: "center",
     justifyContent: "center",
     borderWidth: 1,
     borderColor: "black",
   },
 
-  buttons: {
+  title: {
+    color: theme.colors.darkGreen,
+    textAlign: "center",
+  },
+
+  principal: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+
+  secondary: {
+    fontSize: 28,
+  },
+
+  containerButtons: {
     alignItems: "center",
     justifyContent: "center",
     borderWidth: 1,
     borderColor: "black",
+    gap: 20,
+  },
+
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: theme.colors.lightGreen,
+    color: theme.colors.white,
+    width: 325,
+    padding: 20,
+    borderRadius: 20,
+    fontSize: 16,
   },
 });
 
